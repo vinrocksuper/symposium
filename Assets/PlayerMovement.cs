36139b400e9b8a13bad64ad7359f289bbb20ea96
BT_1N3_02 <kkhambhla0604@bths.edu>
2019-02-15 10:10:12 -0500

    a
    a

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontal = 0f;
+    public float speed = 400f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    //Move character
+    void FixedUpdate()
+    {
+        controller.Move(horizontal*Time.fixedDeltaTime,false,false);
+    }
+}
