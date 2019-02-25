d7beb7f90898e9be5642d438fca1966742335df5
BT_1N3_02 <vli8558@bths.edu>
2019-02-25 10:12:19 -0500

    Beep boop
    a

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     float horizontal = 0f;
+    bool jump = false;
     public float speed = 400f;
 
     // Start is called before the first frame update
@@ -17,12 +18,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        jump = false;
         horizontal = Input.GetAxisRaw("Horizontal") * speed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            Debug.Log("space was pressed");
+        }
     }
 
     //Move character
     void FixedUpdate()
     {
-        controller.Move(horizontal*Time.fixedDeltaTime,false,false);
+        controller.Move(horizontal*Time.fixedDeltaTime,false,jump);
     }
 }
