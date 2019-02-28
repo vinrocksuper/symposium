5ee59867303c3a626b717e647beb2bd0b2dc97fb
Jack Hoang <jhoang6606@bths.edu>
2019-02-28 10:11:17 -0500

    Item

---
+++ Assets/InventoryAndItemScripts/LostItemReaction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostItemReaction : DelayedReaction
+{
+    public item item;
+
+    private inventory inventory;
+    
+    protected override void SpecificInit()
+    {
+        inventory = FindObjectOfType<inventory>();
+    }
+
+}
