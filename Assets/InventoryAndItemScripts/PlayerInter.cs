6f202e64f2a9b6246aa3aef27e0943fe75ca951c
Jack Hoang <jhoang6606@bths.edu>
2019-03-01 10:08:49 -0500

    Interactions
    Interactions

---
+++ Assets/InventoryAndItemScripts/PlayerInter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInter : MonoBehaviour
+{
+
+    public GameObject curInterObj = null;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interObject") && Input.GetButtonUp("Interact"))
+        {
+            Debug.Log(other.name);
+            curInterObj = other.gameObject;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("interObject") && Input.GetButtonUp("Interact"))
+        {
+            Debug.Log(other.name);
+            curInterObj = other.gameObject;
+        }
+    }
+}
